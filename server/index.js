19c448ee20be7589ba232e63597267109e3debf9
Dillan McIlvenna <mcilvenna-d1@ulster.ac.uk>
2022-03-31 20:29:22 +0100

    WIP

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log("There's a new connection!");
+
+    socket.on('disconnect', () => {
+        console.log("A user has disconnected!");
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
